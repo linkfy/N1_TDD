76780258ff22ac7db7d895aad9874c87628c21ae
= <linkstudios@gmail.com>
2026-08-11 19:05:13 +0400

    Implement pattern table decoder

--- emulator/ppu/chr_decoder.py
+++ emulator/ppu/chr_decoder.py
@@ -1,5 +1,11 @@
+PATTERN_TABLE_SIZE = 0x1000
+CHR_TILE_SIZE = 16
+PATTERN_TABLE_TILE_COUNT = 256
+
+# Type alias
+PatternTile = list[list[int]]
 # https://www.nesdev.org/wiki/PPU_pattern_tables
-def decode_chr_tile(tile_bytes: bytes) -> list[list[int]]:
+def decode_chr_tile(tile_bytes: bytes) -> PatternTile:
     """A tile is 16 bytes Bit 0 in first plane, Bit 1 in second plane
     8 Rows for the first plane
     8 Rows for the second plane
@@ -30,17 +36,32 @@ def decode_chr_tile(tile_bytes: bytes) -> list[list[int]]:
         columns = []
         for col in range(COL_SIZE):
             bit_position = 7 - col
-            low = (low_byte >> bit_position) & 1 # Convert to 1 bit | Example 0b1000_000 -> 0b1
+            low = (low_byte >> bit_position) & 1 # Convert to 1 bit. Example: reading bit 7 0b1000_0000 -> gives 1
             high = (high_byte >> bit_position) & 1 # Convert to 1 bit
             pixel = (high << 1) | low
             columns.append(pixel)
 
         rows.append(columns)
     return rows
-            
 
-        
+# Type alias:
+PatternTable = list[PatternTile]
+def decode_pattern_table(pattern_table_bytes: bytes) -> PatternTable:
+    """
+    A pattern table contains 256 pattern tiles, each tile has 16 bytes
+    Total pattern table size = 4096 (0x1000)
+    """
+    if len(pattern_table_bytes) != PATTERN_TABLE_SIZE:
+        raise ValueError("Pattern table must be 4096 bytes")
 
-    
+    tiles = []
 
-    
+    for tile_index in range(PATTERN_TABLE_TILE_COUNT):
+        start = tile_index * CHR_TILE_SIZE
+        end = start + CHR_TILE_SIZE
+        tiles.append(decode_chr_tile(pattern_table_bytes[start:end]))
+
+    return tiles
+
+
+ 
